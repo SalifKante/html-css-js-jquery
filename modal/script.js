0aa820d672558c900bcc170158d7d01591c409d5
Salif Kanté <kantesalif1920@gmail.com>
2024-02-25 17:42:45 +0000

    modal finished

--- modal/script.js
+++ modal/script.js
@@ -1,32 +1,35 @@
 "use strict";
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
 
-const btnsOpenModal = document.querySelectorAll(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
 
-const openModal = function () {
-  //console.log("clicked");
+const btnsModal = document.querySelectorAll(".show-modal");
+
+const showModal = function () {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
-const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-};
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", openModal);
-}
+const closeModal = function () {
+  if (
+    !modal.classList.contains("hidden") &&
+    !overlay.classList.contains("hidden")
+  ) {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  }
+};
+// console.log(btnsModal);
+for (let i = 0; i < btnsModal.length; i++)
+  btnsModal[i].addEventListener("click", showModal);
 
 btnCloseModal.addEventListener("click", closeModal);
-
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (e) {
-  console.log(e);
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-    // console.log("Escape is pressed");
+  // console.log(e);
+  if (e.key === "Escape") {
     closeModal();
   }
 });
